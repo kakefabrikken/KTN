3a919a96c55ca06e9b09aaacb22f07aa3f6f8f59
Kristian Fladstad Normann <krisfn89@gmail.com>
2014-02-23 15:59:17 +0100

    Added code constructing the server

--- Ex1/socket.py
+++ Ex1/socket.py
@@ -1,15 +1,26 @@
 #import socket module 
 from socket import *
-serverSocket = socket(AF_INET, SOCK_STREAM) 
-#Prepare a sever socket 
-#Fill in start 
-#Fill in end 
+try:
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+except socket,error:
+    print 'Failed to create socket'
+    sys.exit()
+print 'socket created'
+
+host = serverSocket.gethostname()
+port = 80 # possibly uneccessary
+serverSocket.bind( host, port )
+
+serverSocket.listen(5) # argument is backlog, 0-5, specifying max number of queued connections
+
+#Prepare a server socket
+
 while True: 
     #Establish the connection 
     print 'Ready to serve...'
     connectionSocket, addr = #Fill in start #Fill in end 
     try: 
-        message = #Fill in start #Fill in end 
+        message = "GET / HTTP / 1.1 " # possibly a little more here 
         filename = message.split()[1] 
         f = open(filename[1:]) 
         outputdata = #Fill in start #Fill in end 
@@ -17,8 +28,8 @@ while True:
         #Fill in start 
         #Fill in end 
         #Send the content of the requested file to the client 
-        for i in range(0, len(outputdata)): 
-        connectionSocket.send(outputdata[i]) 
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i]) 
         connectionSocket.close() 
     except IOError: 
         #Send response message for file not found
