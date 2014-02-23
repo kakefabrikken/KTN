b23c836d0de884edd4ee66345c0a6564bac4cc24
Kristian Fladstad Normann <krisfn89@gmail.com>
2014-02-23 18:35:49 +0100

    May have finished code. Seems to be a prroblem  with py 2.7 - 3.0 difference, str

--- Ex1/socket.py
+++ Ex1/socket.py
@@ -6,36 +6,38 @@ except socket.error:
     sys.exit()
 print ('socket created')
 
-host = gethostbyname(gethostname())
+host = 'localhost' #gethostbyname(gethostname())
 port = 80 # possibly uneccessary
 serverSocket.bind( (host, port) )
 
 serverSocket.listen(1) # argument is backlog, 0-5, specifying max number of queued connections
 
+awesome_msg = ("HTTP / 1.0 200 OK/ \r\n Content-Type: text/html\r\n\r\n")
+failuremsg = ("HTTP / 1.0 404 Not Found\r\n Content-Type: text/plain\r\n\r\n 404 File Not Found")
+
 while True: 
     #Establish the connection 
     print ('Ready to serve...')
-    connectionSocket, addr = serverSocket.accept()
+    conn, addr = serverSocket.accept()
     print ('Got connection from ', addr)
     try:
         # receive data
-        incoming_message = serverSocket.recv(1024) # possibly a little more here 
+        incoming_message = conn.recv(1024) # possibly a little more here 
 
         # process data
-        filename = incoming_message.split()[1] 
-        f = open(filename[1:]) 
+        filename = incoming_message.split()[1]
+        print (filename)
+        f = open(filename[1:])
         outputdata = f.read() 
         #Send one HTTP header line into socket
 
-        awesome_msg = ''
-        connectionSocket(awesome_msg)
+        conn.send(awesome_msg)
         for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i]) 
-        connectionSocket.close() 
+            conn.send(outputdata[i]) 
+        conn.close() 
     except IOError: 
         #Send response message for file not found
-        failuremsg = ''
-        connectionSocket.send(failuremsg)
-        connectionSocket.close()
+        conn.send(failuremsg)
+        conn.close()
 
 serverSocket.close() 
