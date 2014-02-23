8fcd893107d78da3e20281eb63e21738768e998f
Kristian Fladstad Normann <krisfn89@gmail.com>
2014-02-23 17:55:08 +0100

    Made the proper html file and made connection code server side

--- Ex1/socket.py
+++ Ex1/socket.py
@@ -1,43 +1,41 @@
-#import socket module 
 from socket import *
 try:
-    serverSocket = socket(AF_INET, SOCK_STREAM)
-except socket,error:
-    print 'Failed to create socket'
+    serverSocket = socket(AF_INET, SOCK_STREAM) # TCP
+except socket.error:
+    print ("Failed to create socket")
     sys.exit()
-print 'socket created'
+print ('socket created')
 
-host = serverSocket.gethostname()
+host = gethostbyname(gethostname())
 port = 80 # possibly uneccessary
-serverSocket.bind( host, port )
+serverSocket.bind( (host, port) )
 
-serverSocket.listen(5) # argument is backlog, 0-5, specifying max number of queued connections
-
-#Prepare a server socket
+serverSocket.listen(1) # argument is backlog, 0-5, specifying max number of queued connections
 
 while True: 
     #Establish the connection 
-    print 'Ready to serve...'
-    connectionSocket, addr = #Fill in start #Fill in end 
-    try: 
-        message = "GET / HTTP / 1.1 " # possibly a little more here 
-        filename = message.split()[1] 
+    print ('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    print ('Got connection from ', addr)
+    try:
+        # receive data
+        incoming_message = serverSocket.recv(1024) # possibly a little more here 
+
+        # process data
+        filename = incoming_message.split()[1] 
         f = open(filename[1:]) 
-        outputdata = #Fill in start #Fill in end 
-        #Send one HTTP header line into socket 
-        #Fill in start 
-        #Fill in end 
-        #Send the content of the requested file to the client 
+        outputdata = f.read() 
+        #Send one HTTP header line into socket
+
+        awesome_msg = ''
+        connectionSocket(awesome_msg)
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i]) 
         connectionSocket.close() 
     except IOError: 
         #Send response message for file not found
-        #print '404 Not Found'
-        #Fill in start 
-        #Fill in end 
-        #Close client socket 
-        #Fill in start 
-        #Fill in end
-        print 'Gah'
+        failuremsg = ''
+        connectionSocket.send(failuremsg)
+        connectionSocket.close()
+
 serverSocket.close() 
