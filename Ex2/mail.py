142eb23c1af4c6d01c1a623692a0545693898d5f
Kristian Fladstad Normann <krisfn89@gmail.com>
2014-02-24 12:13:03 +0100

    Added screenshots for ex1 and some code in ex2

--- Ex2/mail.py
+++ Ex2/mail.py
@@ -1,24 +1,56 @@
 from socket import *
+
+client_msgs = []
+msg_index = 0
+def client_send_and_recv(index):
+    clientSocket.send(client_msgs[index])
+    recv = clientSocket.recv(1024)
+    print recv
+    if recv[:3] != '250':
+        print '250 reply not received from server'
+
+def client_msgs_handler(msg):
+    client_msgs.append(msg)
+    client_send_and_recv(msg_index)
+    msg_index += 1
+
 msg = '\r\n I love computer networks!'
 endmsg = '\r\n.\r\n'
 # Choose a mail server (e.g. Google mail server) and call it mailserver
-mailserver = #Fill in start   #Fill in end
+mailserver = 'localhost' # possibly something like smtp.stud.ntnu.no
+clientSocket = socket(AF_INET, SOCK_STREAM)
 # Create socket called clientSocket and establish a TCP connection with mailserver
-#Fill in start
+mailport = 25 # 25 may be server side, so this could be wrong
+clientSocket.connect((mailserver, mailport))
 
-#Fill in end
 recv = clientSocket.recv(1024)
 print recv
 if recv[:3] != '220':
 	print '220 reply not received from server.'
 
 # Send HELO command and print server response.
-heloCommand = 'HELO Alice\r\n'
-clientSocket.send(heloCommand)
-recv1 = clientSocket.recv(1024)
-print recv1
-if recv1[:3] != '250':
-	print '250 reply not received from server.'
+#heloCommand = 'HELO Alice\r\n'
+
+client_msg_handler('HELO Alice\r\n')
+
+#client_msgs.append(heloCommand)
+#client_send_and_recv(msg_index)
+#msg_index += 1
+
+
+#clientSocket.send(heloCommand)
+#recv1 = clientSocket.recv(1024)
+#print recv1
+#if recv1[:3] != '250':
+#	print '250 reply not received from server.'
+
+client_msg_handler("MAIL FROM: <krinorm@stud.ntnu.no>\r\n")
+#from_mail = "MAIL FROM: <krinorm@stud.ntnu.no>\r\n"
+
+#clientSocket.send(from_mail)
+
+
+
 
 # Send MAIL FROM command and print server response.
 # Fill in start
